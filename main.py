ef4eb5b74ca5e2d88e1c9110597a9a9a033ddb6c
VihaanChhabria <chhabria.vihaan@gmail.com>
2024-07-15 12:18:16 -0400

    added prompt to get email to send

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import google.generativeai as genai
 
-APIKey = open("/home/vihaan/keys/GeminiAPIKey.txt").read()[:-1]
+APIKEY = open("/home/vihaan/keys/GeminiAPIKey.txt").read()[:-1]
 
-genai.configure(api_key=APIKey)
+def generateEmail(company):
+    def callGemini(content):
+        genai.configure(api_key=APIKEY)
 
-model = genai.GenerativeModel(model_name='gemini-1.5-flash')
-response = model.generate_content('What is AI?')
+        model = genai.GenerativeModel(model_name='gemini-1.5-flash')
+        response = model.generate_content(content)
 
-print(response.text)
+        return response.text
+    content = f"Make an email to {company} asking for a sponsorship for FRC team 7414. My name is Vihaan. Describe what FRC is. Benefits are having your logo on the robot, recognition on our team social media platforms, and many more. Also add how their company's field is related to FRC. Dont add any 'insert here' things. Social media is https://www.instagram.com/perkiomenvalleyrobotics/ and https://www.facebook.com/FRC7414/. For adding links you do not just add the link. JUST RETURN THE EMAIL."
+
+    return callGemini(content)
+
+print(generateEmail("JBT"))
