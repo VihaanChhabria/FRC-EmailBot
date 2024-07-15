5f6e3965fbf3d8ef478fb98f8fc79ddf3f4b6d91
VihaanChhabria <chhabria.vihaan@gmail.com>
2024-07-15 16:07:31 -0400

    email generation is good

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import google.generativeai as genai
 
-APIKEY = open("/home/vihaan/keys/GeminiAPIKey.txt").read()[:-1]
+API_KEY = open("/home/vihaan/keys/GeminiAPIKey.txt").read()[:-1]
 
 def generateEmail(company):
-    def callGemini(content):
-        genai.configure(api_key=APIKEY)
+    def callGemini(prompt):
+        genai.configure(api_key=API_KEY)
 
-        model = genai.GenerativeModel(model_name='gemini-1.5-flash')
-        response = model.generate_content(content)
+        model = genai.GenerativeModel(model_name='gemini-1.5-pro')
+        response = model.generate_content(prompt)
 
         return response.text
-    content = f"Make an email to {company} asking for a sponsorship for FRC team 7414. My name is Vihaan. Describe what FRC is. Benefits are having your logo on the robot, recognition on our team social media platforms, and many more. Also add how their company's field is related to FRC. Dont add any 'insert here' things. Social media is https://www.instagram.com/perkiomenvalleyrobotics/ and https://www.facebook.com/FRC7414/. For adding links you do not just add the link. JUST RETURN THE EMAIL."
+    prompt = f"Make an email to {company} asking for a sponsorship for FRC team 7414. Make it so i can copy it and send it without needing to edit or substitute anything. DO NOT ADD ANY SUBSTITUTE PLACES SUCH AS '[your name]'. My name is Vihaan. Describe what FRC is. Benefits are having your logo on the robot, recognition on our team social media platforms, and many more. Also add how their company's field is related to FRC. Social media is https://www.instagram.com/perkiomenvalleyrobotics/ and https://www.facebook.com/FRC7414/. For adding links you do not just add the link. Dont use any *s to bold"
+    return callGemini(prompt)
 
-    return callGemini(content)
-
-print(generateEmail("JBT"))
